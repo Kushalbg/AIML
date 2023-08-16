54f7e4b2976e29d4900b0e9c8c61af700d960eea
Kushalbg <kushalbandi8@gmail.com>
2023-08-16 14:49:41 +0530

    change1

---
+++ array1.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([[1,2,3],[4,3,5]])
+print("array is of type=", type(arr))
+print("shape of the array=",arr.shape)
+print("dimention of the array=",arr.ndim)
+print("size of the array=",arr.size)
+print("data type of the array=",arr.dtype)
+

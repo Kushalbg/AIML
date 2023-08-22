5667d314f928728b864d095bb5360ab42ae0d263
Kushalbg <kushalbandi8@gmail.com>
2023-08-22 16:32:49 +0530

    change1

---
+++ dataframe.py
@@ -0,0 +1,8 @@
+import pandas as pd
+d={'name':['x','y','z'],'age':[11,22,33]}
+df=pd.DataFrame(d)
+print(df)
+
+data=pd.DataFrame([[1,2,3,4],[2,4,3,1],[2,1,3,4]],columns=['jan','feb','mar','apr'])
+print(data)
+

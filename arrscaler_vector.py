2aec7702b8d69e6097bbd56630c0aa7c47bad039
Kushalbg <kushalbandi8@gmail.com>
2023-09-05 15:27:41 +0530

    change

---
+++ arrscaler_vector.py
@@ -0,0 +1,18 @@
+import numpy as np
+o=np.zeros(3)
+print(o)
+arr=np.ones(5)
+print(arr)
+arr1=np.ones(3).reshape(3,1)
+print(arr1)
+
+m=np.array([[1,2],[3,4],[5,6]])
+print(m)
+print()
+print(np.transpose(m))
+
+print(np.diag([1,3,2]))
+print()
+print(np.identity(3))
+
+
